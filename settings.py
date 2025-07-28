8172e0222ed710ab8c251f919ccda6ef41eef011
Abdulladif Mohamoud <abdulladif16@gmail.com>
2025-07-28 17:24:46 -0400

    Created a setting file and changed the background to be an image scaled to the screen

---
+++ settings.py
@@ -0,0 +1,11 @@
+
+from pathlib import Path
+
+class Settings:
+
+    def __init__(self) -> None:
+        self.name: str = "Alien Invasion"
+        self.screen_w = 1200
+        self.screen_h = 800
+        self.fps = 60
+        self.bg_file = Path.cwd() / 'unit_11_alien_Invasion_starter' / 'Assets' / 'images' / 'Starbasesnow.png'
