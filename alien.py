cab5babeefae02a337908f7f2c8b4da60a27b863
Abdulladif Mohamoud <abdulladif16@gmail.com>
2025-08-02 07:15:01 -0400

    P2- Made alien move across the screen

--- alien.py
+++ alien.py
@@ -20,8 +20,15 @@ class Alien(Sprite):
         self.rect.x = x
         self.rect.y = y
 
+        self.x = float(self.rect.y)
+        self.y = float(self.rect.y)
+
     def update(self):
-        pass
+       
+        temp_speed = self.settings.fleet_speed
+        self.x += temp_speed
+        self.rect.x = self.x
+
 
     def draw_alien(self):
         self.screen.blit(self.image, self.rect)
